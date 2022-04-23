62cd03e1289f322f285fcc4d3d80618f8ed9dc45
Leonardo <josue.aguilar1995@gmail.com>
2022-04-23 13:57:09 -0500

    added block color change utility

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
 var currentDate = moment().format("MMMM Do YYYY");
+var currentHour = moment().format("h");
+var saveDiv = document.getElementsByClassName("save");
 
 // hourly object for text
 var schedule = {
-  eight: "",
-  nine: "",
-  ten: "",
-  eleven: "",
-  twelve: "",
-  one: "",
-  two: "",
-  three: "",
-  four: "",
-  five: "",
+  8: "",
+  9: "",
+  10: "",
+  11: "",
+  12: "",
+  1: "",
+  2: "",
+  3: "",
+  4: "",
+  5: "",
 };
 
 var setCurrentDate = function () {
@@ -39,10 +41,19 @@ $(".hour-text").on("blur", "textarea", function () {
   // get the parent div id
   var hour = $(this).closest(".hour-text").attr("id");
 
+  // set text to value in schedule object
   schedule[hour] = text;
 
   saveSchedule();
+
+  // recreate p element and replace
+  var hourP = $("<p>").addClass("h-50 text-white").text(text);
+  $(this).replaceWith(hourP);
 });
+
+// var saveHandler = function (event) {
+//   console.log(event.target);
+// };
 
 var saveSchedule = function () {
   localStorage.setItem("schedule", JSON.stringify(schedule));
@@ -51,15 +62,60 @@ var saveSchedule = function () {
 var loadSchedule = function () {
   schedule = JSON.parse(localStorage.getItem("schedule"));
 
+  if (!schedule) {
+    schedule = {
+      8: "",
+      9: "",
+      10: "",
+      11: "",
+      12: "",
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+      5: "",
+    };
+  }
+
   Object.keys(schedule).forEach((hour) => {
     var hourTextElement = document.getElementById(hour);
-    // debugger;
+
     hourTextElement.firstElementChild.textContent = schedule[hour];
-    // hourTextElement.textContent = schedule[hour];
   });
+};
+
+var auditHour = function (hourEl) {
+  // get hour from hour ID
+  var hourBlock = hourEl.id;
+
+  // find difference between current hour and hourBlock
+  var hourDifference = hourBlock - currentHour;
+
+  // remove old classes
+  $(hourBlock).removeClass("bg-secondary");
+
+  // change background color based on time
+  if (hourDifference > 0) {
+    $(hourEl).addClass("bg-success");
+  } else if (hourDifference === 0) {
+    $(hourEl).addClass("bg-danger");
+  } else if (hourDifference < 0) {
+    $(hourEl).addClass("bg-secondary");
+  }
 };
 
 setInterval(setCurrentDate, 1000 * 60);
 
+$(".hour-text").each(function (el) {
+  console.log(el);
+});
+
+// setInterval(function () {
+//   $(".hour-text").each(function (el) {
+//     auditHour(el);
+//   });
+// }, 1000);
+
+// saveDiv.addEventListener("click", saveHandler);
 setCurrentDate();
 loadSchedule();
